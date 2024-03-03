4faf83179dd28347cb09b638bd5abb0f15738f25
Arnika12 <arnikagunjal19@gmail.com>
2024-03-03 15:24:20 +0530

    Added dashboard.php

---
+++ admin pannel/dashboard.php
@@ -0,0 +1,16 @@
+<style type="text/css">
+    <?php  include 'style.css'; ?>
+</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- box icon cdn link -->
+    <link href=' ' rel='stylesheet'>
+    <title>Admin - Dashboard</title>
+</head>
+<body>
+    
+</body>
+</html>
