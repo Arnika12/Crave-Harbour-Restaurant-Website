ac58f522a75d77de7b7a9bcbd90fea8b90ae7e4f
Arnika12 <arnikagunjal19@gmail.com>
2024-03-04 00:03:34 +0530

    did necessary changes for admin pages

--- admin pannel/admin_register.php
+++ admin pannel/admin_register.php
@@ -7,8 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- box icon cdn link -->
-    <link href=' ' rel='stylesheet'>
-    <title>Admin - Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" integrity="sha512-pVCM5+SN2+qwj36KonHToF2p1oIvoU3bsqxphdOIWMYmgr4ZqD3t5DjKvvetKhXGc/ZG5REYTT6ltKfExEei/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Admin - Registration Page </title>
 </head>
 <body>
     <?php include '../components/admin_header.php'; ?>
--- admin pannel/dashboard.php
+++ admin pannel/dashboard.php
@@ -13,12 +13,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- box icon cdn link -->
-    <link href='' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" integrity="sha512-pVCM5+SN2+qwj36KonHToF2p1oIvoU3bsqxphdOIWMYmgr4ZqD3t5DjKvvetKhXGc/ZG5REYTT6ltKfExEei/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Admin - Dashboard</title>
 </head>
 <body>
     <div class="main-container">
-    <?php  include '../components/admin_header.php'; ?>
+        <?php  include '../components/admin_header.php'; ?>
     </div>
 
     <?php  include '../components/dark.php'; ?>
