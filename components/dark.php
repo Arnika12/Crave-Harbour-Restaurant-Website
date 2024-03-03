941cc9068ccb85138f7c1e29f74163a33d5dcb52
Arnika12 <arnikagunjal19@gmail.com>
2024-03-03 12:37:08 +0530

    Added dark & light theme code file

--- components/dark.php
+++ components/dark.php
@@ -1,3 +1,42 @@
-<?php
-    
-?>
+
+<div class="day-night">
+    <i class="bx"></i>
+</div>
+<script>
+    const datNight = document.querySelector('.day-night');
+
+    datNight.addEventListener('click', () => {
+        document.body.classList.contains("dark");
+        if(document.body.classList.contains("dark")){
+            localStorage.setItem("theme", "dark");
+        }
+        else{
+            localStorage.setItem("theme", "light");
+        }
+        updateIcon();
+    })
+
+    function themeMode(){
+        if(localStorage.setItem("theme") !== null){
+            if(localStorage.setItem("theme") === "light"){
+                document.body.classList.remove("dark");
+            }
+            else{
+                document.body.classList.add("dark");
+            }
+        }
+        updateIcon();
+    }
+    themeMode();
+
+    function updateIcon(){
+        if(document.body.classList.contains("dark")){
+            datNight.querySelector("i").classList.add("bxs-sun");
+            datNight.querySelector("i").classList.remove("bxs-moon");
+        }
+        else{
+            datNight.querySelector("i").classList.remove("bxs-sun");
+            datNight.querySelector("i").classList.add("bxs-moon");
+        }
+    }
+</script>
