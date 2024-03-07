4af64f656c2445169584b41901ead0dcab0cf215
Arnika12 <arnikagunjal19@gmail.com>
2024-03-07 20:21:22 +0530

    added home.php

---
+++ home.php
@@ -0,0 +1,36 @@
+
+<?php
+    include 'components/connect.php'; 
+    session_start();
+
+    if(isset($_SESSION['user_id'])){
+        $user_id = $_SESSION['user-id'];
+    }else{
+        $user_id = '';
+    }
+?>
+
+<style type="text/css">
+    <?php  include 'style.css'; ?>
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- box icon cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" integrity="sha512-pVCM5+SN2+qwj36KonHToF2p1oIvoU3bsqxphdOIWMYmgr4ZqD3t5DjKvvetKhXGc/ZG5REYTT6ltKfExEei/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Crave Harbour - Home Page</title>
+</head>
+<body>
+    <?php include 'components/user_header.php'; ?>
+
+    <?php include 'components/dark.php'; ?>
+    <!-- sweetalert cdn link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- custom js link -->
+    <script type="text/javascript" src="script.js"></script>
+    <?php include 'components/alert.php'; ?>
+</body>
+</html>
